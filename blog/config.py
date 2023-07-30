004742ab7c000792c36f387c7330504b9bac90a0
Jenkins CI <jenkins-ci@jamiehurst.co.uk>
2023-07-30 14:30:07 +0000

    Skip CI: updated version number

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,4 @@ from os import environ
 
 SECRET_KEY = environ.get('SECRET_KEY', default='dev')
 ARTICLES_DIR = environ.get('ARTICLES_DIR', default='articles/')
-VERSION = 'v0.18.1'
+VERSION = 'v0.18.1.1'
