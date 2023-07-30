0f5541ab25049a4debd86f4b7d5379a0ded1f240
Jamie Hurst <jamie@jamiehurst.co.uk>
2023-07-30 15:03:01 +0100

    Remove GitHub version call

--- blog/config.py
+++ blog/config.py
@@ -2,6 +2,4 @@ from os import environ
 
 SECRET_KEY = environ.get('SECRET_KEY', default='dev')
 ARTICLES_DIR = environ.get('ARTICLES_DIR', default='articles/')
-GITHUB_USERNAME = environ.get('GITHUB_USERNAME', default='')
-GITHUB_TOKEN = environ.get('GITHUB_TOKEN', default='')
-VERSION = 'DEVELOPMENT'
+VERSION = 'v0.18'
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,27 +4,9 @@ from blog import create_app
 
 
 @pytest.fixture
-def mock_github_request_empty_response(requests_mock):
-    requests_mock.get(
-        'https://api.github.com/repos/jamiefdhurst/blog/releases',
-        json='',
-    )
-
-
-@pytest.fixture
-def mock_github_request(requests_mock):
-    requests_mock.get(
-        'https://api.github.com/repos/jamiefdhurst/blog/releases',
-        json=[{'name': 'v1.0-TEST'}],
-    )
-
-
-@pytest.fixture
 def app():
     app = create_app({
         'ARTICLES_DIR': 'tests/articles/',
-        'GITHUB_USERNAME': 'jamiefdhurst',
-        'GITHUB_TOKEN': 'example',
         'TESTING': True,
         'VERSION': 'DEVELOPMENT',
     })
