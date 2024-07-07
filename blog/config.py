5ce2873facef4146fdbb4ebd2d0620c0f63d0f03
jamiefdhurst <jamiefdhurst@users.noreply.github.com>
2024-07-07 10:35:43 +0000

    [skip ci] Update version to v0.24.11

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,4 @@ from os import environ
 
 ARTICLES_DIR = environ.get('ARTICLES_DIR', default='articles/')
 DIST_DIR = environ.get('DIST_DIR', default='dist/')
-VERSION = 'v0.24.10'
+VERSION = 'v0.24.11'
