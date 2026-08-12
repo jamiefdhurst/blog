e306208b0074173db8ba2e3a4a3ba2799e91baf8
jamiefdhurst <524681+jamiefdhurst@users.noreply.github.com>
2026-08-12 10:22:16 +0000

    [skip ci] Update version to v1.8.0

--- blog/config.py
+++ blog/config.py
@@ -3,4 +3,4 @@ from os import environ
 SITE_URL = environ.get('SITE_URL', default='https://jamiehurst.co.uk')
 ARTICLES_DIR = environ.get('ARTICLES_DIR', default='articles/')
 DIST_DIR = environ.get('DIST_DIR', default='dist/')
-VERSION = 'v1.7.0'
+VERSION = 'v1.8.0'
